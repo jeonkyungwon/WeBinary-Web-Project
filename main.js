229f224d1e990d81034bc99a511b24d2a8d94a94
minbirdkk <kimminse0513@naver.com>
2023-02-26 17:17:36 +0900

    commit message

--- main.js
+++ main.js
@@ -3,46 +3,60 @@ let canvas;
 let ctx; //캔버스를 만드는 데 도움을 준다.
 canvas = document.createElement("canvas")
 ctx = canvas.getContext("2d")
-canvas.width=1920;
-canvas.height=1080;
+canvas.width=2200;
+canvas.height=1060;
 document.body.appendChild(canvas);
+
+
+function loadImage(){
+    sball = new Image();
+    sball.src = 'soccerball.png'
+
+    skeeper = new Image();
+    skeeper.src = 'goalkeeper.png'
+
+    divingkeeper = new Image();
+    divingkeeper.src = 'rrdivingkeeper.png'
+}
 
 //필요 소품: 축구공, 골대, 키퍼
 var ball={  //축구공
     x : 1100,
     y : 900,
-    width : 50,
-    height : 50,
+    width : 80,
+    height : 80,
     draw(){
-        ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        //ctx.drawImage(sball, this.x, this.y);
+        ctx.drawImage(sball, this.x, this.y, this.width, this.height);
     }
 }
 
 var net = {  //골대
-    x : 500,
-    y : 10,
-    width : 1200,
-    height : 400,
+    x : 340,
+    y : 30,
+    width : 1590,
+    height : 550,
     draw(){
-        ctx.fillStyle = 'yellow';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
 var keeper = {  //keeper
     x : 1025,
     y : 200,
-    width : 150,
-    height : 300,
-    draw(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        //ctx.drawImage(skeeper,this.x, this.y, this.width, this.height);
+    width : 200,
+    height : 500,
+    readydraw(){
+        ctx.drawImage(skeeper,this.x, this.y, this.width, this.height);
     }
 }
-
+var dkeeper = {
+    x : 1025,
+    y : 200,
+    width : 200,
+    height : 500,
+    draw(){
+        ctx.drawImage(divingkeeper, this.x, this.y, this.width, this.height);
+    }
+}
 var goal={
     x : 700,
     y : 200,
@@ -54,10 +68,15 @@ var goal={
     }
 }
 
-function render(){  //render 함수로 그려주기
-    net.draw()
-    keeper.draw()
-    ball.draw()
+var nogoal={
+    x : 700,
+    y : 200,
+    width : 800,
+    height : 100,
+    draw(){
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 
 //2. 움직임
@@ -67,15 +86,20 @@ function render(){  //render 함수로 그려주기
 function random(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function movekeeper(){
+function jumpkeeper(){
+    ctx.clearRect(keeper.x, keeper.y, keeper.width, keeper.height)
     const xrand = random(-525,525)
     const yrand = random(0,190)
     keeper.x+=xrand
+    dkeeper.x += xrand
     keeper.y-=yrand
+    dkeeper.y -= yrand
 }
 
 //키보드 이벤트
 let keysDown = {};
+var move = 0;
+let power = 0, side = 0;
 function keyboardeventforkeeper(){
     document.addEventListener('keydown', function(e){
         keysDown[e.keyCode] = true;
@@ -84,110 +108,128 @@ function keyboardeventforkeeper(){
     document.addEventListener('keyup', function(e){
         delete keysDown[e.keyCode];
         
-        if(e.keyCode == 37 || e.keyCode == 39){
-            ctx.clearRect(1025,200, 150, 300);
-            movekeeper()
-
-            if(e.keyCode == 37){
-                updateballleft()
+        if(e.keyCode == 37 || e.keyCode == 39 || e.keyCode == 38 || e.keyCode == 40){
+            
+            if(e.keyCode == 38){
+                if(power >= 0 && power <= 2){
+                    power+=1;
+                }
             }
-    
-            if(e.keyCode == 39){
-                updateballright()
+            
+            if(e.keyCode == 40){
+                if(power >= 1 && power <= 3){
+                    power-=1;
+                }
             }
 
+            if(e.keyCode == 37){  //left
+                if(side >= -2 && side <= 3){
+                    side-=1;
+                }
+            }
+
+            if(e.keyCode == 39){ //right
+                if(side >= -3 && side <= 2){
+                    side+=1;
+                }
+            }
+            
         }
-        if(e.keyCode == 37 || e.keyCode == 39){
-            collision(ball, keeper)
+
+        if(e.keyCode == 32){
+            jumpkeeper()
+            updateball()
+        }
+
+        if(e.keyCode == 32){
+            collision(ball, dkeeper, net)
         }
     })
 }
 
-//2-2. 공 차기
-// let keysarr = {};
-// function keyboardeventforball(){
-//     document.addEventListener('keydown', function(event){
-//         keysarr[event.keyCode] = true;
-//     })
-    
-//     document.addEventListener('keyup', function(event){
-//         delete keysarr[event.keyCode];
-        
-//         if(event.keyCode == 37){
-//             updateballleft()
-//         }
-
-//         if(event.keyCode == 39){
-//             updateballright()
-//         }
-//     })
-// }
 
 //xy는 그냥 난수 생성해서 해보면 될 듯?
 let rball;
-function updateballleft(){
-    rball=requestAnimationFrame(updateballleft)
+var lr = 0
+function updateball(){
     ctx.clearRect(ball.x, ball.y, ball.width+50, ball.height+50);
-    ball.x -= 100;
-    ball.y -= 120;
-    console.log(ball.x, ball.y)
-    if(ball.x <= 510 || ball.y <= 10){
-        cancelAnimationFrame(rball)
+    if(side == 0){
+        ball.y -= 450 + 100*(power+1);
     }
-}
-
-let lball;
-function updateballright(){
-    lball=requestAnimationFrame(updateballright)
-    ctx.clearRect(ball.x, ball.y, ball.width+50, ball.height+50);
-    ball.x += 100;
-    ball.y -= 120;
-    console.log(ball.x, ball.y)
-    if(ball.x+50 >= 1650 || ball.y <= 10){
-        cancelAnimationFrame(lball)
+    else{
+        ball.x += 240*side;
+        ball.y -= 450 + 100*(power+1);
     }
 }
 
 //3. collision check
-var goalcheck = 0;
-function collision(ball, keeper){
-    var x축차이 = ball.x - (keeper.x + keeper.width);
-    var y축차이 = ball.y - (keeper.y + keeper.height);
-
-    if(x축차이 < 0 && y축차이 < 0){
-        console.log('NO Goal!')
-        console.log(ball.x, ball.y, keeper.x + keeper.width, keeper.y + keeper.height)
-        goalcheck = 0;
+var goa = 0, kee = 0, check = 0;
+function collision(ball, keeper, net){
+    check = 1;
+    
+    //공과 골대
+    if(ball.x > net.x && ball.x+ball.width < net.x+net.width){
+        if(ball.y > net.y && ball.y + ball.height < net.y + net.height){
+            goa += 1;
+        }
     }
-    else{
-        console.log('jjGoal!')
-        console.log(ball.x, ball.y, keeper.x + keeper.width, keeper.y + keeper.height)
-        goalcheck = 1;
+    
+    //공과 골키퍼
+    if((dkeeper.x <= ball.x+ ball.width && ball.x+ ball.width <= dkeeper.x + dkeeper.width) ||
+    (dkeeper.x <= ball.x && ball.x <= dkeeper.x + dkeeper.width)){
+        kee += 1;
+    }
+    if((ball.y <= dkeeper.y && dkeeper.y<= ball.y+ ball.height) || (dkeeper.y <= ball.y && ball.y <= dkeeper.y + dkeeper.height)){
+        kee += 1;
     }
 }
 
+//4. 택스트
+function cleartext(){
+    ctx.clearRect(1680, 920, canvas.width-1680, canvas.height-920);
+} 
+function text(){
+    ctx.font = "italic bold 30px Arial, sans-serif";
+    ctx.fillSytle = 'Black'
+    ctx.fillText("POWER:" +power, 1800, 920);
+    ctx.fillText("SIDE:" +side, 1680, 920);
+}
+function render(){  //render 함수로 그려주기
+    net.draw()
+    keeper.readydraw()
+    ball.draw()
+}
+function arender(){
+    net.draw()
+    dkeeper.draw()
+    ball.draw()
+}
 //main함수 -> 반복할 거
 function main(){  //main 함수
     requestAnimationFrame(main)
-    render()
-    if(goalcheck == 1){
-        goal.draw()
-        cancelAnimationFrame(main)
+    text()
+    if(check == 1){
+        arender()
+        if(goa == 1 && kee <= 1){
+            goal.draw()
+        }
+        else{
+            nogoal.draw()
+        }
+        //check = 0;
     }
+    else{
+        render()
+    }
+    cancelAnimationFrame(main)
 }
-    
+
     
 //함수 호출
-//loadImage()
+loadImage()
 keyboardeventforkeeper()
 
 main()
 
 
-
-//1.볼이 다 업데이트 되고 나서 골판독을 해야 한다.
-//2. 볼의 슛 강도, 방향 자유롭게 하기.
-
-//경원이 형이 골 카운트
-
-//원준이가 그림 그려주면서 html 버튼 만들기?
+////Tlqkf fuck cleartext
